91bfbd8c7ef6bc752921b2f8ca299dba8df304ac
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-07-28 21:25:27 +0200

    complete exercises 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (strangoo, numeroo) {
+  let outputoo = "";
+  if (numeroo < 0) {return "ERROR"}
+  for (i = 0; i < numeroo; i++) {
+    outputoo += strangoo;
+  }
+  return outputoo;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function (stringoo) {
+  let lenghtoo = stringoo.length;
+  let output ="";
 
+
+  for (i = 0; i < lenghtoo; i++) {
+    output = stringoo.substr(i,1).concat(output);
+  }
+  return output;
 };
+
+// let stringoo = "This is a string";
+// let lenghtoo = stringoo.length;
+// let output = "";
+// for (i = 0; i < lenghtoo; i++) {
+//    output = stringoo.substr(i,1).concat(output);
+//      console.log(output)
+//  }
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
