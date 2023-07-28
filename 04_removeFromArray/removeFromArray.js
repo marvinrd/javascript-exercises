4431c0e45aed88fa2d12f4abbd17004dd8cd7e72
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-07-28 23:16:28 +0200

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  for (i = 0; i < arguments.length; i++) {
+    for (j = 0; j < arguments[0].length; j++) {
+      if (arguments[0][j] === arguments[i]) {
+        arguments[0].splice(j, 1);
+      }
+    }
+  }
 
+  return arguments[0];
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
