5e57d182350070cbf810c70595a545e299fcbf79
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-07-31 22:24:13 +0200

    Done 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let output = ((temp - 32) * 5) / 9;
+  
+    return Math.round(output*10)/10;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let output = (temp * 9) / 5 + 32;
+  
+    return Math.round(output*10)/10;
+  
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
