729b14cd258db83db27743ae448f441bc7491998
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-07-31 21:43:20 +0200

    Done 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(inputOne, inputTwo) {
+    sum = 0;
 
+    if (typeof inputOne != 'number' || typeof inputTwo != 'number' || inputOne < 0 || inputTwo < 0)
+    {return 'ERROR'}
+
+    if (inputOne > inputTwo) {
+        let temp = inputOne;
+        inputOne = inputTwo;
+        inputTwo = temp;
+    }
+
+    for ( inputOne ; inputOne  < inputTwo +1 ; inputOne++)
+    {
+        sum = sum + inputOne;
+    }
+    return sum;
 };
 
 // Do not edit below this line
