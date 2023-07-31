2beda9bfe15485376487e5716d7f481b3013f807
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-07-31 21:54:42 +0200

    Done 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // divide by 100
+  // if there is a remainder, check if year is divisible by 4
+  // if there is no remainder. check if year is divisible by 400
 
+  if (year % 100 != 0) {
+    if (year % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (year % 100 === 0) {
+    if (year % 400 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  else {return false}
 };
 
 // Do not edit below this line
