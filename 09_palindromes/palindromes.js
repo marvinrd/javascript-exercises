381a291480c430610fcfa5d3b38322dae6274abd
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-08-01 14:20:25 +0200

    Done 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
 const palindromes = function () {
+  let countChars = arguments[0].length;
+  let origString = arguments[0].toLowerCase();
+  let leftToRight = "";
+  let rightToLeft = "";
 
+  for (i = 0; i < countChars; i++) {
+    if (
+      (origString.charCodeAt(i) > 96 && origString.charCodeAt(i) < 123) ||
+      (origString.charCodeAt(i) > 47 && origString.charCodeAt(i) < 58)
+    ) {
+      leftToRight = leftToRight + origString[i];
+      rightToLeft = origString[i] + rightToLeft;
+    }
+  }
+  let output = leftToRight === rightToLeft;
+  return output;
 };
 
 // Do not edit below this line
