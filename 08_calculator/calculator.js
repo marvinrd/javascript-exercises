ad7c3dfb48a9793f1309f7b7affcf5e60cb62d18
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-08-01 13:40:10 +0200

    Done 07 & 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function () {
+  let count = arguments.length;
+  let output = 0;
+  for (i = 0; i < count; i++) {
+    output = output + arguments[i];
+  }
+  return output;
 };
 
-const subtract = function() {
-	
+const subtract = function () {
+  let output = arguments[0] - arguments[1];
+
+  return output;
 };
 
-const sum = function() {
-	
+const sum = function () {
+  let count = arguments[0].length;
+  let output = 0;
+  for (i = 0; i < count; i++) {
+    output = output + arguments[0][i];
+  }
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function () {
+  let count = arguments[0].length;
+  let output = 1;
+  for (i = 0; i < count; i++) {
+    output = output * arguments[0][i];
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function () {
+  let output = Math.pow(arguments[0], arguments[1]);
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function () {
+  let output = 1;
+  if (arguments[0] === 0 || arguments[0] === 1) {
+    return output;
+  } else {
+    for (i = 1; i < arguments[0] + 1; i++) {
+      output = output * i;
+    }
+    return output;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +55,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
