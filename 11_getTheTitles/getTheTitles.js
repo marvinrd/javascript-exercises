54b9a180ab02d560ad62c90744725e3b15285d5b
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-08-07 20:26:51 +0200

    complete all exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,6 @@
-const getTheTitles = function() {
+const getTheTitles = function (array) {
+  let output = array.map(input => input.title);
+  return output;
 
 };
 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  let oldestPerson = {};
 
+  array.reduce((maxAge, person) => {
+    let ageCurrent = 0;
+
+    if (!person.yearOfDeath) {
+      ageCurrent = (new Date().getFullYear()) - person.yearOfBirth;
+    } else {
+      ageCurrent = person.yearOfDeath - person.yearOfBirth;
+    }
+
+    if (maxAge < ageCurrent) {
+      oldestPerson = person;
+      return ageCurrent;
+    } else {
+      return maxAge;
+    }
+  }, 0);
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
